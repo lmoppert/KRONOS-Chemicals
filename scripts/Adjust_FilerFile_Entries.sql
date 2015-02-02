17c4235143811841d8f37a1b5c0e3217dfe53475
Lutz Moppert <lutz.moppert@kronosww.com>
2015-02-02 18:11:53 +0100

    SQL scripts now reflect the renaming of the DB

--- scripts/Adjust_FilerFile_Entries.sql
+++ scripts/Adjust_FilerFile_Entries.sql
@@ -1,2 +1,2 @@
-UPDATE `intranet`.`filer_file` SET `owner_id` = 1, `name` = LEFT(`original_filename`, CHAR_LENGTH(`original_filename`)-4) WHERE `id` > 100;
-SELECT * FROM intranet.filer_file;
+UPDATE `she`.`filer_file` SET `owner_id` = 1, `name` = LEFT(`original_filename`, CHAR_LENGTH(`original_filename`)-4) WHERE `id` > 100;
+SELECT * FROM she.filer_file;
