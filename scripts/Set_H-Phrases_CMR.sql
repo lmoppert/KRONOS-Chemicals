b1109e72bbaa5e9fa84dbaf5d76bf61efe240329
Lutz Moppert <lutz.moppert@kronosww.com>
2015-03-03 15:28:51 +0100

    Added script that sets the CMR value on the H-params

---
+++ scripts/Set_H-Phrases_CMR.sql
@@ -0,0 +1,17 @@
+UPDATE she.chemicals_hphrase SET cmr = 1
+    WHERE id = 76
+    OR id = 78
+    OR id = 79
+    OR id = 81
+    OR id = 82
+    OR id = 83
+    OR id = 84
+    OR id = 85
+    OR id = 86;
+UPDATE she.chemicals_hphrase SET cmr = 2
+    WHERE id = 77
+    OR id = 80
+    OR id = 87
+    OR id = 88
+    OR id = 89
+    OR id = 90;
