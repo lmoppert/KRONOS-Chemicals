c263a7355ce4ca4f683ca49985d61ac072a30464
Lutz Moppert <lutz.moppert@kronosww.com>
2015-09-02 10:02:59 +0200

    Made tranlations.py more pythonic

--- translation.py
+++ translation.py
@@ -1,63 +1,50 @@
 """ Model Translations for Chemicals."""
 
 from modeltranslation.translator import translator, TranslationOptions
-from .models.chemical import (HPhrase, PPhrase, RPhrase, Pictogram, WGK, Risk,
-                               SevesoCategory, StorageClass, ReachInformation,
-                               SevesoInformation, Chemical, Synonym,
-                               RiskIndication, HPhraseRelation)
-from .models.periphery import Plant
-from .models.checklist import CheckUsage, CheckSection, PPE
+from chemicals import models
 
 
 class NameTranslationOptions(TranslationOptions):
-
     """Translating only the name field."""
-
     fields = ('name',)
 
 
-translator.register(Plant, NameTranslationOptions)
-translator.register(Pictogram, NameTranslationOptions)
-translator.register(Synonym, NameTranslationOptions)
-translator.register(RiskIndication, NameTranslationOptions)
+translator.register(models.Plant, NameTranslationOptions)
+translator.register(models.Pictogram, NameTranslationOptions)
+translator.register(models.Synonym, NameTranslationOptions)
+translator.register(models.RiskIndication, NameTranslationOptions)
 
 
 class DescriptionTranslationOptions(TranslationOptions):
-
     """Translating only the description field."""
-
     fields = ('description',)
 
 
-translator.register(StorageClass, DescriptionTranslationOptions)
-translator.register(ReachInformation, DescriptionTranslationOptions)
-translator.register(SevesoInformation, DescriptionTranslationOptions)
-translator.register(CheckUsage, DescriptionTranslationOptions)
-translator.register(CheckSection, DescriptionTranslationOptions)
-translator.register(PPE, DescriptionTranslationOptions)
-translator.register(WGK, DescriptionTranslationOptions)
-translator.register(SevesoCategory, DescriptionTranslationOptions)
-translator.register(HPhrase, DescriptionTranslationOptions)
-translator.register(PPhrase, DescriptionTranslationOptions)
-translator.register(RPhrase, DescriptionTranslationOptions)
+translator.register(models.StorageClass, DescriptionTranslationOptions)
+translator.register(models.ReachInformation, DescriptionTranslationOptions)
+translator.register(models.SevesoInformation, DescriptionTranslationOptions)
+translator.register(models.CheckUsage, DescriptionTranslationOptions)
+translator.register(models.CheckSection, DescriptionTranslationOptions)
+translator.register(models.PPE, DescriptionTranslationOptions)
+translator.register(models.WGK, DescriptionTranslationOptions)
+translator.register(models.SevesoCategory, DescriptionTranslationOptions)
+translator.register(models.HPhrase, DescriptionTranslationOptions)
+translator.register(models.PPhrase, DescriptionTranslationOptions)
+translator.register(models.RPhrase, DescriptionTranslationOptions)
 
 
 class InfoTranslationOptions(TranslationOptions):
-
     """Translating only the info field."""
-
     fields = ('info',)
 
 
-translator.register(Risk, InfoTranslationOptions)
-translator.register(HPhraseRelation, InfoTranslationOptions)
+translator.register(models.Risk, InfoTranslationOptions)
+translator.register(models.HPhraseRelation, InfoTranslationOptions)
 
 
 class ChemicalTranslationOptions(TranslationOptions):
-
     """Translating both, name and comment field."""
-
     fields = ('name', 'comment',)
 
 
-translator.register(Chemical, ChemicalTranslationOptions)
+translator.register(models.Chemical, ChemicalTranslationOptions)
